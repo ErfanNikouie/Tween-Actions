e0d123addb33c024a6518b8c226dc68ccd8335cf
Erfan Nikouie <er.nikouie@gmail.com>
2023-12-07 10:53:11 +0330

    Settings changed.

--- Assets/Mosaic/Base/TweenActions/Responsive/ResponsiveTweenActionSettings.cs
+++ Assets/Mosaic/Base/TweenActions/Responsive/ResponsiveTweenActionSettings.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Mosaic.Base.TweenActions
 {
-	[CreateAssetMenu(fileName = "Responsive Settings", menuName = "TweenAction/Responsive Settings", order = 0)]
+	[CreateAssetMenu(fileName = "Responsive Settings", menuName = "TweenAction/Responsive Tween Settings", order = 0)]
 	public class ResponsiveTweenActionSettings : ScriptableObject
     {
         [SerializeField] private Vector2 referenceResolution = new Vector2(1920, 1080);
